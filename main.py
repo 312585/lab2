7f16286380a0bc3257da069bd71145b2338c806c
312585 <kate_01222@mail.ru>
2020-11-29 14:44:07 +0300

    Исправлены ошибки нэйминга

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
-file = open('steam (1).csv', 'r', encoding='utf-8')
-mainList = []
-counter = 2
-headers0 = file.readline().split(',')
-headers = headers0[2::]
+file = open('steam (1).csv', encoding='utf-8')
+MainList = []
+column = 2
+FullHeaders = file.readline().split(',')
+headers = FullHeaders[2:]
 lines = file.readlines()
 for line in lines:
-    splitedStr = line.split(',')
-    splitedElems = []
-    for elem in splitedStr:
-        splitedElems.append(elem.split(';'))
-    mainList.append(splitedElems)
-for colomnName in headers:
-    fileteredList = []
-    print('Введите ', colomnName)
-    insertParams = input().split(',')
-    for param in mainList:
-        if bool(set(param[counter]) & set(insertParams)) or insertParams[0] == '':
-            fileteredList.append(param)
-    counter += 1
-    mainList = fileteredList
+    SplitedStr = line.split(',')
+    SplitedElems = []
+    for elem in SplitedStr:
+        SplitedElems.append(elem.split(';'))
+    MainList.append(SplitedElems)
+for ColumnName in headers:
+    FileteredList = []
+    print('Введите ', ColumnName)
+    InsertParams = input().split(',')
+    for param in MainList:
+        if bool(set(param[column]) & set(InsertParams)) or InsertParams[0] == '':
+            FileteredList.append(param)
+    column += 1
+    MainList = FileteredList
 file.close()
 
 results = open('result', 'w', encoding='utf-8')
-for param in mainList:
-    counter = 0
+for param in MainList:
+    column = 0
     results.write('Вам подходит игра: \n')
-    for elem in headers0:
-        results.write(elem+' : ')
-        for m in param[counter]:
-            results.write(m+' ')
+    for elem in FullHeaders:
+        results.write(elem + ' : ')
+        for m in param[column]:
+            results.write(m + ' ')
         results.write('\n')
-        counter += 1
+        column += 1
 results.close()
